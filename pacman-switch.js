8849db4dccee630090e37eb50290893e11b41133
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-26 16:00:55 -0400

    complete assignment

---
+++ pacman-switch.js
@@ -0,0 +1,19 @@
+var ghost = 'clyde';
+var colour
+
+switch (ghost){
+  case 'inky':
+    colour = 'cyan';
+    break;
+  case 'blinky':
+    colour = 'red';
+    break;
+  case 'pinky':
+    colour = 'pink';
+    break;
+  case 'clyde':
+    colour = 'orange';
+    break;
+}
+
+console.log(ghost + ' is the colour ' + colour);
